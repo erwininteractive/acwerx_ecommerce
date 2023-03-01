469ad943c9c0c8e2f94c80cd5da70ad31a76a418
Andrew S Erwin <andrew@erwininteractive.com>
2023-03-01 14:20:58 -0600

    fixed loading error

--- _types/common-types/index.d.ts
+++ _types/common-types/index.d.ts
@@ -1,5 +1,6 @@
-declare module 'common-types' {
+import { Image, Slug } from "sanity"
 
+declare module 'common-types' {
     type Base = {
         _createdAt: string
         _id: string
@@ -8,42 +9,11 @@ declare module 'common-types' {
         _updatedAt: string
     }
 
-    interface Name {
-        _type: 'string'
-        current: string
-    }
-
-    interface Slug {
-        _type: 'slug'
-        current: string
-    }
-
-    interface Image {
-        _type: 'image'
-        asset: Reference
-    }
-
-    interface Description {
-        _type: 'text'
-        current: string
-    }
-
-    interface Price {
-        _type: 'number'
-        current: string
-    }
-
-    interface Reference {
-        _ref: string
-        _type: 'reference'
-    }
-
     interface Product extends Base {
         name: string
         slug: Slug
         image: Image
-        description: Text
+        description: string
         price: Number
     }
-
 }
--- components/Products.tsx
+++ components/Products.tsx
@@ -1,16 +1,3 @@
+import Image from 'next/image'
 import type { Product } from 'common-types'
 
-type Props = {
-    products: Product[]
-}
-
-export default function Products({ products }: Props) {
-    return (
-        <>
-            {/* Products */}
-            {products.map(product => (
-                <p key='_id'>{ product.name }</p>
-            ))}
-        </>
-    )
-}
